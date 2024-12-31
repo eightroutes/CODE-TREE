5bef9b794ef875a234d569fdb87ab552ee74ab65
EllGHT <52287330+eightroutes@users.noreply.github.com>
2024-12-31 22:20:43 +0900

    Title: 특정 숫자 도달하기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241231/특정 숫자 도달하기/reaching-specific-number.py
+++ 241231/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,14 +1,12 @@
 arr = list(map(int, input().split()))
 
 sum = 0
-index = 0
 
 for i in range(10):
     if(arr[i] >= 250):
         break
     sum+=arr[i]
-    index = i
 
-average = float(sum/i)
+average = round(sum/i, 1)
 
 print(sum, average)
