4a655c32ed582acd05774308c87a5daab076786e
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-01-04 18:27:52 +0900

    Title: 점수대 파악하기, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250104/점수대 파악하기/find-out-the-score-range.py
+++ 250104/점수대 파악하기/find-out-the-score-range.py
@@ -1,15 +1,16 @@
 arr = list(map(int, input().split()))
 
 grade_arr = [100-10*i for i in range(10)]
+count_arr = [0]*10
 
-print(grade_arr)
 
 for elem in arr:
     if elem == 0:
         break
     idx = grade_arr.index(elem//10*10) 
-    grade_arr[idx] += 1
+    count_arr[idx] += 1
+    
 
 for i in range(10):
-    print(f"{(100-10*i) - grade_arr[i]}")
+    print(f"{grade_arr[i]} - {count_arr[i]}")
     
