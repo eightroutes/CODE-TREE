231eea892e472cf163c615cdf5940eebfb150db3
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-01-04 14:28:19 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 38ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250104/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input().split()))
+
+
+for elem in arr:
+    if elem == 0:
+        zeroIdx = arr.index(elem)
+
+sum = arr[zeroIdx-1]+arr[zeroIdx-2]+arr[zeroIdx-3]
+
+print(sum)
