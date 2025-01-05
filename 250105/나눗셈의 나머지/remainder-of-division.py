2eafdce52748872eeb47588adebcf57b35081691
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-01-05 22:11:21 +0900

    Title: 나눗셈의 나머지, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250105/나눗셈의 나머지/remainder-of-division.py
+++ 250105/나눗셈의 나머지/remainder-of-division.py
@@ -1,11 +1,11 @@
 a, b = map(int, input().split())
 
-rm_arr = [0]*9
+rm_arr = [0]*10
 
 while a > 1:
-    a = a // b
     rm = a % b
     rm_arr[rm] += 1
+    a = a//b
 
 res = [elem*elem for elem in rm_arr]
 
