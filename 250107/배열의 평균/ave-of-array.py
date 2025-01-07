886380395f0bdc23ec3a27049edfce41ae8852ba
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-01-07 22:15:30 +0900

    Title: 배열의 평균, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250107/배열의 평균/ave-of-array.py
+++ 250107/배열의 평균/ave-of-array.py
@@ -10,15 +10,15 @@ res3 = 0
 for i in range(2):
     for j in range(4):
         res1 += arr[i][j]
-    print(res1/4, end=" ")
+    print(round(res1/4,1), end=" ")
     res1 = 0
 print()
 
 for i in range(4):
     res2 = (arr[0][i] + arr[1][i]) / 2
-    print(res2, end=" ")
+    print(round(res2,1), end=" ")
 print()
 
 res3 = (sum(arr[0]) + sum(arr[1])) / 8
-print(res3)
+print(round(res3,1))
 
