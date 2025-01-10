28ac899f356679546ee571b67bfcc2d3fed0a086
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-01-10 17:12:42 +0900

    Title: 문자열 범위 출력하기 2, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250110/문자열 범위 출력하기 2/print-string-in-range-2.py
+++ 250110/문자열 범위 출력하기 2/print-string-in-range-2.py
@@ -3,6 +3,9 @@ n = int(input())
 
 st_reverse = st[::-1]
 
-for i in range(0, n):
-    print(st_reverse[i],end="")
+if n > len(st):
+    print(st_reverse)
+else:
+    for i in range(0, n):
+        print(st_reverse[i],end="")
 
