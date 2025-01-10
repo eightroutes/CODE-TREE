f7796dcee8e0325f4c1b6935bbb0d276ea0d73bb
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-01-10 18:27:17 +0900

    Title: Run Length 인코딩, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250110/Run Length 인코딩/run-length-encoding.py
+++ 250110/Run Length 인코딩/run-length-encoding.py
@@ -3,14 +3,13 @@ A = input()
 # Write your code here!
 arr = []
 cnt = 1
+arr.append(A[0])
 for i in range(1, len(A)):
     if A[i-1] == A[i]:
         cnt+=1
         if A[i] not in arr:
             arr.append(A[i-1])
     elif A[i-1] != A[i]:
-        if(arr==None):
-            arr.append(A[i-1])
         arr.append(str(cnt))
         cnt = 1
         arr.append(A[i])
