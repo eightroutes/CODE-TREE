4dd0a064f3d8dcb2d458a649c3082c7b3fc5acc8
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-01-13 16:39:52 +0900

    Title: 정수만 추출하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250113/정수만 추출하기/extract-only-integers.py
+++ 250113/정수만 추출하기/extract-only-integers.py
@@ -3,13 +3,20 @@ a, b = input().split()
 int_a = 0 
 int_b = 0
 
+if a.isdigit() == True:
+    int_a = int(a)
+elif b.isdigit() == True:
+    int_b = int(b)
+
 for i in range(len(a)):
     if a[i].isdigit() == False:
         int_a = int(a[:i])
         break
+    
 for i in range(len(b)):
     if b[i].isdigit() == False:
         int_b = int(b[:i])
         break
+    
 
 print(int_a + int_b)
