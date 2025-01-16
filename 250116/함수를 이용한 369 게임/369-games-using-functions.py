c4994b19bc947242d861795cffc5581ac1a60153
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-01-16 14:31:42 +0900

    Title: 함수를 이용한 369 게임, Time: 84ms, Memory: 18MB, Status: Passed - CodeTree

--- 250116/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250116/함수를 이용한 369 게임/369-games-using-functions.py
@@ -2,22 +2,21 @@ a, b = map(int, input().split())
 
 # Write your code here!
 
-def count_third(n):
+def is_third(n):
     if n % 3 == 0:
         return True
     while n > 0:
-        digit = n % 10
-        if digit in (3, 6, 9):
+        if n % 10 in [3, 6, 9]:
             return True
         n //= 10
-    return False
 
-def from_to(a, b):
+def count_third(a, b):
     sum_v = 0
     for i in range(a, b+1):
-        if count_third(i):
-            sum_v+=1
+        if is_third(i):
+            sum_v += 1
     return sum_v
 
-print(from_to(a,b))
 
+print(count_third(a,b))
+    
